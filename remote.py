b913acde5720ed3ba71b37c3569f9293d162ee6c
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-30 10:14:16 -0500

    Add buttons for each person and clock in/out toggle

--- remote.py
+++ remote.py
@@ -1,8 +1,18 @@
 from gpiozero import Button
-
+import googleForm 
 
 hotSwitch = Button(26)
+clockInOut = Buton(19)
+julio = Buton(16)
+miguel = Buton(20)
+pedro = Buton(21)
 
 while True:
-  if hotSwitch.is_pressed:
-    print('On')
+  if True:
+    if clockInOut.value is True:
+      #googleForm.submitClockIn('Julio')
+      print('clockIn')
+
+    elif clockInOut.value is False:
+      #googleForm.submitClockOut('Julio')
+      print('clockOut')
