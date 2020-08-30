8d727457ce2a1bffd7d6631c577a7f25dc5d116c
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-30 12:53:23 -0500

    Test when_pressed

--- remote.py
+++ remote.py
@@ -7,9 +7,16 @@ julio = Button(16)
 miguel = Button(20)
 pedro = Button(21)
 
+state = False
+def toggle(state):
+  if state == False:
+    state = True
+  else:
+    state = False
 while True:
   while hotSwitch.is_active == True:
-    print('HotSwitch')
+    clockInOut.when_pressed(toggle(state))
+    print('Current state', state)
 # if clockInOut.is_active == True and julio.is_pressed:
 #   print('Julio clockIn')
 # if clockInOut.is_active == True and miguel.is_pressed:
