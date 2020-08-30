c8fba8b967c7582ed789028b9a99918bef207b6e
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-30 12:43:40 -0500

    Test is_active

--- remote.py
+++ remote.py
@@ -7,19 +7,20 @@ julio = Button(16)
 miguel = Button(20)
 pedro = Button(21)
 
-while True:
-  if clockInOut.is_active == True and julio.is_pressed:
-    print('Julio clockIn')
-  if clockInOut.is_active == True and miguel.is_pressed:
-    print('Miguel clockIn')
-  if clockInOut.is_active == True and pedro.is_pressed:
-    print('Pedro clockIn')
+while hotSwitch.is_active == True:
+  print('HotSwitch')
+# if clockInOut.is_active == True and julio.is_pressed:
+#   print('Julio clockIn')
+# if clockInOut.is_active == True and miguel.is_pressed:
+#   print('Miguel clockIn')
+# if clockInOut.is_active == True and pedro.is_pressed:
+#   print('Pedro clockIn')
 
-  if clockInOut.is_active == False and julio.is_pressed:
-    print('Julio clockIn')
-  if clockInOut.is_active == False and miguel.is_pressed:
-    print('Miguel clockIn')
-  if clockInOut.is_active == False and pedro.is_pressed:
-    print('Pedro clockIn')
+# if clockInOut.is_active == False and julio.is_pressed:
+#   print('Julio clockIn')
+# if clockInOut.is_active == False and miguel.is_pressed:
+#   print('Miguel clockIn')
+# if clockInOut.is_active == False and pedro.is_pressed:
+#   print('Pedro clockIn')
 
 
