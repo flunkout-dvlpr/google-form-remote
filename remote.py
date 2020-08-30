2e99b09e3072781b46ad1ba75605f17c1da4a019
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-30 12:20:50 -0500

    Remove outer if

--- remote.py
+++ remote.py
@@ -8,23 +8,22 @@ miguel = Button(20)
 pedro = Button(21)
 
 while True:
-  if True:
-    if clockInOut.value is True:
-      if julio.is_pressed:
-        print('Julio clockIn')
-      if miguel.is_pressed:
-        print('Miguel clockIn')
-      if pedro.is_pressed:
-        print('Pedro clockIn')
-      print('idle')
+  if clockInOut.value is True:
+    if julio.is_pressed:
+      print('Julio clockIn')
+    if miguel.is_pressed:
+      print('Miguel clockIn')
+    if pedro.is_pressed:
+      print('Pedro clockIn')
+    print('idle')
 
-    elif clockInOut.value is False:
-      #googleForm.submitClockOut('Julio')
-      if julio.is_pressed:
-        print('Julio clockOut')
-      if miguel.is_pressed:
-        print('Miguel clockOut')
-      if pedro.is_pressed:
-        print('Pedro clockOut')
-      print('idle')
+  elif clockInOut.value is False:
+    #googleForm.submitClockOut('Julio')
+    if julio.is_pressed:
+      print('Julio clockOut')
+    if miguel.is_pressed:
+      print('Miguel clockOut')
+    if pedro.is_pressed:
+      print('Pedro clockOut')
+    print('idle')
 
